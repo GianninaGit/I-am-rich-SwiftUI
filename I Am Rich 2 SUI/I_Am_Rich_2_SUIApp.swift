fb8b586ec48efb0f0a6a49151e57ab52f48a557a
Giannina <giannisarappa@gmail.com>
2023-10-28 20:38:21 +0200

    First commit

---
+++ I Am Rich 2 SUI/I_Am_Rich_2_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_2_SUIApp.swift
+//  I Am Rich 2 SUI
+//
+//  Created by Giannina on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_2_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
